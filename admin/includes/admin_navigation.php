7dbb295b5bf7f25fbee89744c5a1a35eb112abd8
FarrukhMahkamov <farruhmahkamov20@gmail.com>
2021-03-10 16:30:27 +0500

    profile done

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -43,7 +43,7 @@
             </a>
           </li>
           <li class="nav-item ">
-            <a class="nav-link" href="./includes/profile.php">
+            <a class="nav-link" href="./profile.php">
               <i class="material-icons">person</i>
               <p>profile</p>
             </a>
